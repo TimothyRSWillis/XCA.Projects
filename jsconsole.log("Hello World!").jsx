5b6a8c8cd248e0abd73954ad40d3867402c53c7f
Timothy Willis <TimothyRSWillis@gmail.com>
2022-04-18 09:07:25 -0400

    work

---
+++ jsconsole.log("Hello World!").jsx
@@ -0,0 +1,53 @@
+console.log("Hello World!")
+
+// Variables! 
+// let value can change
+// const value can not change
+
+const firstName = "Devan"
+const lastName = "Allara"
+
+let age = 30
+
+let isItRaining = true
+
+let number1 = 5
+let number2 = 10
+
+//String Concatination 
+console.log(firstName + " " + lastName + " is " + age + " years old!")
+
+//String Interpolation 
+console.log(`${firstName} ${lastName} is ${age} years old!`)
+
+//Camel Casing
+// firstName
+// firstSecondThird
+
+//JS DataTypes
+String = "Words"
+Number = 43
+Boolean = true
+
+
+//  is devans age 30? if true do stuff : if false do other stuff
+
+
+
+let firstNumber = 4;
+let secondNumber = 9;
+
+firstNumber = firstNumber + secondNumber
+secondNumber = firstNumber - secondNumber
+firstNumber = firstNumber - secondNumber
+
+console.log(`${firstNumber} ${secondNumber}`)
+
+let a = 2;
+let b = 8;
+const c = a + b * b; //66
+const d = a * b + b; //24
+const e = a * (b + b); //32
+const f = a * b / a; //8 
+const g = b / a * a; //8
+console.log(a, b, c, d, e, f, g);
